20dd10cb9b812adb5cb34c4912c1c487583cefdf
Hanson Tang <142570321+hansontang@users.noreply.github.com>
2024-02-26 22:11:30 +0800

    Add files via upload

--- lab3/randomizedtest/BuggyAList.java
+++ lab3/randomizedtest/BuggyAList.java
@@ -57,10 +57,10 @@ public class BuggyAList<Item> {
     }
 
     /** Deletes item from back of the list and
-      * returns deleted item. */
+     * returns deleted item. */
     public Item removeLast() {
         if ((size < items.length / 4) && (size > 4)) {
-            resize(size / 4);
+            resize(items.length / 4);
         }
         Item x = getLast();
         items[size - 1] = null;
